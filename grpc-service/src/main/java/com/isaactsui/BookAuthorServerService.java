e3a308c3c340163e3b6bb598bea8c6841862afcb
Isaac Tsui <isaactsui2000@gmail.com>
2022-08-14 18:32:36 +0800

    Initial commit

---
+++ grpc-service/src/main/java/com/isaactsui/BookAuthorServerService.java
@@ -0,0 +1,12 @@
+package com.isaactsui;
+
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BookAuthorServerService extends BookAuthorServiceGrpc.BookAuthorServiceImplBase {
+    @Override
+    public void getAuthor(Author request, StreamObserver<Author> responseObserver) {
+
+    }
+}
